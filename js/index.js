d45e14aab1c74ddca953ce5b896b5207d122ef23
practical-kernel-3029 <nah1222@yahoo.com>
2020-03-24 00:33:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,9 +10,3 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
-
-if (height === "70") {
-    console.log("The height is correct")
-} else {
-    console.log("Expected the height to be 70")
-}
