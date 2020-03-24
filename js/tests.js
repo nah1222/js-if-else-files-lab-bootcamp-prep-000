14faa05e12615be7719101266703725226cc2511
practical-kernel-3029 <nah1222@yahoo.com>
2020-03-24 00:20:10 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -2,7 +2,7 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} else {
+} else if {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
