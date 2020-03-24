d248677dba6f639f0526b86e43f4c7418bd1fa0e
practical-kernel-3029 <nah1222@yahoo.com>
2020-03-24 00:27:35 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -1,13 +1,12 @@
 var index = require("./index.js")
 
-if (index.name === "Susan", index.height === "70") {
-    console.log("The name is correct")node.js/
-    console.log("The height is correct")
+if (index.name === "Susan") {
+    console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
-    console.log("Expected: 70, Received: "+index.height)
   
 }
+
 
 
 
